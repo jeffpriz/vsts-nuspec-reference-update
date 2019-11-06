498db44d856669f62465f5047d0ddfc365d50cc0
Jeff Przylucki <jeffpriz@hotmail.com>
2019-11-06 10:34:08 -0600

    setup test for no nuget dependencies

--- NuspecUpdate/dist/test/testprocess.js
+++ NuspecUpdate/dist/test/testprocess.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var run = require("../taskRun");
-run.DoWork('./NuspecUpdate/src/test/NugetSampleTest.csproj', './NuspecUpdate/src/test/NugetSampleTest.nuspec', false);
+//run.DoWork('./NuspecUpdate/src/test/NugetSampleTest.csproj','./NuspecUpdate/src/test/NugetSampleTest.nuspec',false);
+run.DoWork('./NuspecUpdate/src/test/NugetSampleTestNoRef.csproj', './NuspecUpdate/src/test/NugetSampleTestNoRef.nuspec', false);
 //# sourceMappingURL=testprocess.js.map
--- NuspecUpdate/src/test/testprocess.ts
+++ NuspecUpdate/src/test/testprocess.ts
@@ -2,4 +2,5 @@ import * as run from '../taskRun';
 import * as getFSData from '../getFSData';
 
 
-run.DoWork('./NuspecUpdate/src/test/NugetSampleTest.csproj','./NuspecUpdate/src/test/NugetSampleTest.nuspec',false);
+//run.DoWork('./NuspecUpdate/src/test/NugetSampleTest.csproj','./NuspecUpdate/src/test/NugetSampleTest.nuspec',false);
+run.DoWork('./NuspecUpdate/src/test/NugetSampleTestNoRef.csproj','./NuspecUpdate/src/test/NugetSampleTestNoRef.nuspec',false);
